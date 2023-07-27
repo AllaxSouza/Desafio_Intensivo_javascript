579d2a779b53d4955fed13ec439a341032d03b08
AllaxSouza <allaxsouza@hotmail.com>
2023-07-27 09:49:01 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,86 @@
+// Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert("Hello Word!")
+
+// Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let numberOne = prompt("Digite o primeiro número: ")
+// let numeberTwo = prompt("Digite o segundo número: ")
+
+// numberOne = Number(numberOne)
+// numeberTwo = Number(numeberTwo)
+
+// const sun = numberOne + numeberTwo
+
+// alert( `O resultado da soma é : ${sun}`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// const sub = numberOne - numeberTwo
+
+// alert(`O resultado da subetração é : ${sub}`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// const mult = numberOne * numeberTwo
+
+// alert(`O resultado da multiplicação é ${mult}`)
+
+// Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// const div = numberOne / numeberTwo
+
+// alert(`O resultado da divisão é : ${div}`)
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// let numberOne = prompt("Digite um número: ")
+
+// numberOne = Number(numberOne)
+
+// const restdiv = numberOne % 2
+
+// if (restdiv == 0) {
+
+//     alert(`${numberOne} é um numero par`)
+// } else {
+//   alert(`${numberOne} não é um numero par`)
+// }
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+// if (restdiv != 0) {
+
+//   alert(`${numberOne} é um numero ímpar`)
+// } else {
+// alert(`${numberOne} não é um numero ímpar`)
+// }
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+// let number = "vinte"
+
+// if (typeof number === "number") {
+//   alert(`${number} é um número!` )
+// } else {
+//   alert(`${number} não é um número!`)
+// }
+
+// Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let string = 24
+
+// if (typeof string === "string") {
+//   alert(`${string} é uma string!` )
+// } else {
+//   alert(`${string} não é um string!`)
+// }
+
+// Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// let booleano = true
+
+// if (typeof booleano === true) {
+//   alert(`${booleano} é um booleano!` )
+// } else {
+//   alert(`${booleano} não é um booleano!`)
+// }
